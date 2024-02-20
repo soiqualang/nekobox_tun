dd439d7fdbfecf87f794bcc7c297267fbdeec366
soiqualang <soiqualang.chentreu@gmail.com>
2024-02-20 23:24:32 +0700

    t1

---
+++ Hướng Dẫn Cài Mạng Viettel Miễn Phí Không Cần Nền - Đặng Minh Đông Blog_files/js.php
@@ -0,0 +1,4 @@
+var r = encodeURIComponent(document.referrer); var l = encodeURIComponent(top.document.URL);var wh = encodeURIComponent('?**'+screen.width+'x'+screen.height); document.currentScript.insertAdjacentHTML(
+  'beforebegin', 
+ "<a href='https://cs.u-on.eu/in.php?u=76689'><img src='https://cs.u-on.eu/c.php?u=76689&rjs="+r+wh+"&ljs="+l+"' alt='CS' width='88' height='31'></a>"
+)
